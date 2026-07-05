cb6620542490835d9669d1a07d89f51675f493ef
Erik van `t Wout <erik@ewnetworks.eu>
2026-07-05 02:21:19 +0200

    Simplify popuphook to use a global focus listener

--- src/scriptinjections/popuphook/index.ts
+++ src/scriptinjections/popuphook/index.ts
@@ -1,7 +1,5 @@
 import { openExtensionPopup } from '../../internalapi/requests';
-import { DocumentSearcher } from '../../lib/documentsearcher';
 import { getInjectionContext } from '../context';
-import { hookNewElements } from './hookinjector';
 
 
 const tryOpenPopup = (input: HTMLInputElement) => {
@@ -15,33 +13,8 @@ const tryOpenPopup = (input: HTMLInputElement) => {
     }
 };
 
-const triggerInjection = (_?: MutationRecord[], mutationObserver?: MutationObserver) => {
-    let ctx = getInjectionContext();
-
-    /* 
-     *  Since we ignore the actual observer data and instead directly
-     *  check the DOM, it is safe and even preferred to completely
-     *  flush the mutation event queue.
-     */
-    mutationObserver?.takeRecords();
-    ctx.pageChanges++;
-
-    if (ctx.pageChanges >= 100 && mutationObserver) {
-        console.debug('Excessive page changes detected, stopped hooking new input fields');
-        mutationObserver.disconnect();
-        return;
+document.addEventListener('focus', (event) => {
+    if (event.target && (event.target as any).type === 'password') {
+        tryOpenPopup(event.target as HTMLInputElement);
     }
-
-    if (ctx.injectionTimer)
-        clearTimeout(ctx.injectionTimer);
-
-    ctx.injectionTimer = setTimeout(() =>
-        hookNewElements('onfocus', tryOpenPopup, new DocumentSearcher().getPasswordInputsInDocumentAndIFrames(), ctx.hookedInputs),
-    100);
-};
-
-new MutationObserver(triggerInjection).observe(document, {
-    subtree: true,
-    childList: true
-});
-triggerInjection();
+}, true);
